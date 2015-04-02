c2b16d8ef134dc3d1ad65583727a817408835f43
laissonsilveira <lyon.zimba@gmail.com>
2015-04-02 00:48:21 -0300

    Exclusão dos registro com scores mais baixos de cada estudante

--- hw2-3/src/main/java/course/homework/Homework.java
+++ hw2-3/src/main/java/course/homework/Homework.java
@@ -1,67 +1,52 @@
 package course.homework;
 
+import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Sorts.ascending;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bson.Document;
+
 import com.mongodb.MongoClient;
-import com.mongodb.ServerAddress;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-import freemarker.template.Configuration;
-import freemarker.template.Template;
-import org.bson.Document;
-import spark.Request;
-import spark.Response;
-import spark.Route;
-import spark.Spark;
-
-import java.io.StringWriter;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import static java.util.Arrays.asList;
 
 public class Homework {
-    public static void main(String[] args)  {
-        final Configuration configuration = new Configuration();
-        configuration.setClassForTemplateLoading(Homework.class, "/freemarker");
+    public static void main(String[] args) {
 
-        MongoClient client = new MongoClient();
+	MongoClient client = new MongoClient();
+	MongoDatabase database = client.getDatabase("students");
+	final MongoCollection<Document> collection = database
+		.getCollection("grades");
 
-        MongoDatabase database = client.getDatabase("m101");
-        final MongoCollection<Document> collection = database.getCollection("funnynumbers");
+	List<Document> results = collection.find(eq("type", "homework"))
+		.sort(ascending("student_id", "score"))
+		.into(new ArrayList<Document>());
 
-        Spark.get(new Route("/") {
-            @Override
-            public Object handle(final Request request,
-                                 final Response response) {
-                StringWriter writer = new StringWriter();
-                try {
-                    Template template = configuration.getTemplate("answer.ftl");
+	List<Document> studentsRemove = new ArrayList<Document>();
+	for (Document result : results) {
+	    if (!studentsRemove.isEmpty()) {
+		boolean contains = false;
+		for (Document student : studentsRemove) {
+		    if (student.getDouble("student_id").equals(
+			    result.getDouble("student_id"))) {
+			contains = true;
+			break;
+		    }
+		}
+		if (!contains) {
+		    studentsRemove.add(result);
+		}
+	    } else {
+		studentsRemove.add(result);
+	    }
+	}
 
-                    // Not necessary yet to understand this.  It's just to prove that you
-                    // are able to run a command on a mongod server
-                    List<Document> results =
-                    collection.aggregate(asList(new Document("$group", new Document("_id", "$value")
-                                                                       .append("count", new Document("$sum", 1))),
-                                                new Document("$match", new Document("count", new Document("$lte", 2))),
-                                                new Document("$sort", new Document("_id", 1))))
-                              .into(new ArrayList<Document>());
+	for (Document document : studentsRemove) {
+	    collection.deleteOne(eq("_id", document.get("_id")));
+	}
 
-                    int answer = 0;
-                    for (Document cur : results) {
-                        answer += (Double) cur.get("_id");
-                    }
-
-                    Map<String, String> answerMap = new HashMap<String, String>();
-                    answerMap.put("answer", Integer.toString(answer));
-
-                    template.process(answerMap, writer);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    halt(500);
-                }
-                return writer;
-            }
-        });
+	System.out.println("Done: " + (collection.count() == 600l));
     }
 }
