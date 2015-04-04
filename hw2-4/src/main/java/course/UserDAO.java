9ae49480f5849d463c278282b0611ed38e20a1ac
laissonsilveira <lyon.zimba@gmail.com>
2015-04-04 09:29:35 -0300

    Update tarefa 4 da semana 2

--- hw2-4/src/main/java/course/UserDAO.java
+++ hw2-4/src/main/java/course/UserDAO.java
@@ -16,12 +16,7 @@
 
 package course;
 
-import com.mongodb.ErrorCategory;
-import com.mongodb.MongoWriteException;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
-import org.bson.Document;
-import sun.misc.BASE64Encoder;
+import static com.mongodb.client.model.Filters.eq;
 
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
@@ -29,80 +24,82 @@ import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.util.Random;
 
-import static com.mongodb.client.model.Filters.eq;
+import org.bson.Document;
+
+import sun.misc.BASE64Encoder;
+
+import com.mongodb.ErrorCategory;
+import com.mongodb.MongoWriteException;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 
 public class UserDAO {
     private final MongoCollection<Document> usersCollection;
     private Random random = new SecureRandom();
 
     public UserDAO(final MongoDatabase blogDatabase) {
-        usersCollection = blogDatabase.getCollection("users");
+	usersCollection = blogDatabase.getCollection("users");
     }
 
     // validates that username is unique and insert into db
     public boolean addUser(String username, String password, String email) {
 
-        String passwordHash = makePasswordHash(password, Integer.toString(random.nextInt()));
+	String passwordHash = makePasswordHash(password,
+		Integer.toString(random.nextInt()));
 
-        // XXX WORK HERE
-        // create an object suitable for insertion into the user collection
-        // be sure to add username and hashed password to the document. problem instructions
-        // will tell you the schema that the documents must follow.
+	Document user = new Document("_id", username).append("password",
+		passwordHash);
 
-        if (email != null && !email.equals("")) {
-            // XXX WORK HERE
-            // if there is an email address specified, add it to the document too.
-        }
+	if ((email != null) && !email.equals("")) {
+	    user.append("email", email);
+	}
 
-        try {
-            // XXX WORK HERE
-            // insert the document into the user collection here
-            return true;
-        } catch (MongoWriteException e) {
-            if (e.getError().getCategory().equals(ErrorCategory.DUPLICATE_KEY)) {
-                System.out.println("Username already in use: " + username);
-                return false;
-            }
-            throw e;
-        }
+	try {
+	    usersCollection.insertOne(user);
+	    return true;
+	} catch (MongoWriteException e) {
+	    if (e.getError().getCategory().equals(ErrorCategory.DUPLICATE_KEY)) {
+		System.out.println("Username already in use: " + username);
+		return false;
+	    }
+	    throw e;
+	}
     }
 
     public Document validateLogin(String username, String password) {
-        Document user = null;
 
-        // XXX look in the user collection for a user that has this username
-        // assign the result to the user variable.
+	Document user = usersCollection.find(eq("_id", username)).first();
 
-        if (user == null) {
-            System.out.println("User not in database");
-            return null;
-        }
+	if (user == null) {
+	    System.out.println("User not in database");
+	    return null;
+	}
 
-        String hashedAndSalted = user.get("password").toString();
+	String hashedAndSalted = user.get("password").toString();
 
-        String salt = hashedAndSalted.split(",")[1];
+	String salt = hashedAndSalted.split(",")[1];
 
-        if (!hashedAndSalted.equals(makePasswordHash(password, salt))) {
-            System.out.println("Submitted password is not a match");
-            return null;
-        }
+	if (!hashedAndSalted.equals(makePasswordHash(password, salt))) {
+	    System.out.println("Submitted password is not a match");
+	    return null;
+	}
 
-        return user;
+	return user;
     }
 
-
     private String makePasswordHash(String password, String salt) {
-        try {
-            String saltedAndHashed = password + "," + salt;
-            MessageDigest digest = MessageDigest.getInstance("MD5");
-            digest.update(saltedAndHashed.getBytes());
-            BASE64Encoder encoder = new BASE64Encoder();
-            byte hashedBytes[] = (new String(digest.digest(), "UTF-8")).getBytes();
-            return encoder.encode(hashedBytes) + "," + salt;
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException("MD5 is not available", e);
-        } catch (UnsupportedEncodingException e) {
-            throw new RuntimeException("UTF-8 unavailable?  Not a chance", e);
-        }
+	try {
+	    String saltedAndHashed = password + "," + salt;
+	    MessageDigest digest = MessageDigest.getInstance("MD5");
+	    digest.update(saltedAndHashed.getBytes());
+	    BASE64Encoder encoder = new BASE64Encoder();
+	    byte hashedBytes[] = (new String(digest.digest(), "UTF-8"))
+		    .getBytes();
+	    return encoder.encode(hashedBytes) + "," + salt;
+	} catch (NoSuchAlgorithmException e) {
+	    throw new RuntimeException("MD5 is not available", e);
+	} catch (UnsupportedEncodingException e) {
+	    throw new RuntimeException("UTF-8 unavailable?  Not a chance", e);
+	}
     }
 }
